e74a721ae84f5a135956c0e23ebf7a2cc5455e5d
trino2 <trino2@yahoo.com>
2018-03-27 21:44:43 +0000

    changed the user file to hashtagserch file

--- routes/hashtagSerch.js
+++ routes/hashtagSerch.js
@@ -1,94 +1,34 @@
 var express = require('express');
+var Twit = require('twit');
 var router = express.Router();
-var https = require('https'); 
-var btoa = require('btoa');
 
-var keys = {
-    client: process.env.consumer_key, 
-    secret: process.env.consumer_secret
-};
+var T = new Twit({
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token: process.env.access_token,
+  access_token_secret: process.env.access_token_secret,
+  // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+});
 
-var combined = keys.client + ":" + keys.secret; 
-var base64encoded = btoa(combined);
-var hashtagSearchRe = "funny";
-
-function getAccessToken(handleAccessTokenResponse) {
-    const options = {
-        hostname: "api.twitter.com", 
-        port: 443, 
-        path: '/oauth2/token',
-        method: 'POST', 
-        headers: {
-            'Authorization': 'Basic ' + base64encoded, 
-            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+function twitterData(sendBackResponseToBrowser) {
+    var tweets = '';
+    var target = 'lol';
+    T.get('search/tweets', { q: target, count: 30 }, function(err, data, response) {
+    if (err){
+        console.log("This craped the bed: ", err);
+    }
+    else {
+        console.log("Tweeter send this: ", data);
+        tweets = JSON.stringify(data);
+        sendBackResponseToBrowser(tweets);
         }
-    }; 
-    
-    var postData = 'grant_type=client_credentials'; 
-    var completeResponse = ''; 
-    
-    // Set up the request
-    var postReq = https.request(options, function(res) {
-      res.setEncoding('utf8');
-      res.on('data', function (chunk) {
-          completeResponse += chunk; 
-      });
-      
-      res.on('end', function() {
-            console.log("########################################"); 
-            console.log("status code: " + this.statusCode); 
-            //console.log("Complete response: " + completeResponse); 
-            var responseJSON = JSON.parse(completeResponse); 
-            var accessToken = responseJSON.access_token; 
-            
-            handleAccessTokenResponse(accessToken); 
-      }); 
     });
-    
-    postReq.write(postData);
-    postReq.end();
-}
-
-function getTweets(accessToken, sendResponseToBrowser) {
-    const options = {
-        hostname: "api.twitter.com", 
-        port: 443, 
-        path: '/1.1/search/tweets.json?q=' + hashtagSearchRe,
-        method: 'GET', 
-        headers: {
-            'Authorization': 'Bearer ' + accessToken
-        }
-    }; 
-    
-    var completeResponse = ''; 
-    
-    // Set up the request
-    var twitterRequest = https.request(options, function(twitterResponse) {
-      twitterResponse.setEncoding('utf8');
-      twitterResponse.on('data', function (chunk) {
-          completeResponse += chunk; 
-      });
-      
-      twitterResponse.on('end', function() {
-            console.log("########################################"); 
-            console.log("status code: " + this.statusCode); 
-            
-            var responseJSON = JSON.parse(completeResponse); 
-            var tweetsList = responseJSON.statuses; 
-            sendResponseToBrowser(tweetsList); 
-      }); 
-    });
-    
-    twitterRequest.end();
 }
 
 router.get('/', function(req, res, next) {
-  getAccessToken(function(accessToken) {
-    getTweets(accessToken, function(tweets) {
-        console.log("num tweets: " + tweets.length);
-        res.render('hashtagSerch', {tweets: tweets});
-    }); 
-  }); 
+    twitterData(function(tweets){
+        res.render('hashtagSerch', { title: 'Raw Hashtag Search', tweets: tweets});
+    });
 });
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,34 +1,8 @@
 var express = require('express');
-var Twit = require('twit');
 var router = express.Router();
 
-var T = new Twit({
-  consumer_key: process.env.consumer_key,
-  consumer_secret: process.env.consumer_secret,
-  access_token: process.env.access_token,
-  access_token_secret: process.env.access_token_secret,
-  // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-});
-
-function twitterData(sendBackResponseToBrowser) {
-    var tweets = '';
-    var target = 'lol';
-    T.get('search/tweets', { q: target, count: 30 }, function(err, data, response) {
-    if (err){
-        console.log("This craped the bed: ", err);
-    }
-    else {
-        console.log("Tweeter send this: ", data);
-        tweets = JSON.stringify(data);
-        sendBackResponseToBrowser(tweets);
-        }
-    });
-}
-
 router.get('/', function(req, res, next) {
-    twitterData(function(tweets){
-        res.render('user', { title: 'Hashtag Search', tweets: tweets});
-    });
+    res.render('user', { title: 'Twitters Critter User'});
 });
 
 module.exports = router;
